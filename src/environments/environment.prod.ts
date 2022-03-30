1bdc192da1c6dbb44f945a7a92d0c2c907fd45b9
MarinaAndersen1371 <72156348+MarinaAndersen1371@users.noreply.github.com>
2022-03-30 22:39:54 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
-  RAPIDARY_KEY: '4a8d6fcb72mshe6ae65f00325de5p1e0764jsn89e228caf14f',
-  RAPIDARY_HOST: 'rawg-video-games-database.p.rapidapi.com',
-  PARAMS_KEY: '6cd86721808a4fc98c107a8ff5c5c569'
+
 };
